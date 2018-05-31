5344b34c6352443d84f8218de24acf7365601337
Tyler Damme <tyler.damme@gmail.com>
2018-05-31 15:16:07 -0500

    Simple variables and console work

---
+++ 2-JS-Basics/script.js
@@ -0,0 +1,38 @@
+// var name = 'John';
+// console.log(name);
+
+// var lastName = 'Smith';
+// console.log(lastName);
+
+// var age = 26;
+// console.log(age);
+
+// var fullAge = true;
+// console.log(fullAge);
+
+// Lecture: Variables 2
+
+var name = 'John';
+var age = 26;
+
+console.log(name + age);
+console.log(age + age);
+
+var job, isMarried;
+
+console.log(job); //undefined
+
+job = 'teacher';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+
+age = 'thirty six';
+job = 'driver';
+
+console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+
+var lastName = prompt('What is the last name?');
+console.log(lastName);
+
+alert(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
