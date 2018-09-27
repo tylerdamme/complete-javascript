7eab4a602d2ee408de4b71f95391bf4bb619b441
Tyler Damme <tyler.damme@gmail.com>
2018-09-27 18:52:58 -0500

    operators

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -12,27 +12,89 @@
 
 // Lecture: Variables 2
 
-var name = 'John';
-var age = 26;
+// var name = 'John';
+// var age = 26;
 
-console.log(name + age);
-console.log(age + age);
+// console.log(name + age);
+// console.log(age + age);
 
-var job, isMarried;
+// var job, isMarried;
 
-console.log(job); //undefined
+// console.log(job); //undefined
 
-job = 'teacher';
-isMarried = false;
+// job = 'teacher';
+// isMarried = false;
 
-console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+// console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
 
-age = 'thirty six';
-job = 'driver';
+// age = 'thirty six';
+// job = 'driver';
 
-console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+// console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
 
-var lastName = prompt('What is the last name?');
-console.log(lastName);
+// var lastName = prompt('What is the last name?');
+// console.log(lastName);
 
-alert(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+// alert(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+
+/******************************/
+/* basic operators */
+
+// var year, yearJohn, yearMark;
+// now = 2018;
+// ageJohn = 28;
+// ageMark = 33;
+
+// // math operators
+// yearJohn = now - ageJohn;
+// yearMark = now - ageMark;
+// console.log(yearJohn);
+
+// console.log(now + 2);
+// console.log(now * 2);
+// console.log(now / 10);
+
+// // Logical operators
+
+// var johnOlder = ageJohn < ageMark;
+// console.log(johnOlder);
+
+// // typeof operator
+// console.log(typeof johnOlder);
+// console.log(typeof ageJohn);
+// console.log(typeof "Mark is older than John");
+// var x;
+// console.log(typeof x);
+
+/****************************************/
+// operator precedence
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+// Mulitple operators
+var isFullAge = now - yearJohn >= fullAge; // true
+console.log(isFullAge);
+
+// Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+
+// Multiple assignments
+var x, y;
+x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
+console.log(x, y);
+
+// More operators
+x *= 2;
+console.log(x);
+x += 10;
+console.log(x);
+x++;
+console.log(x);
+x--;
+console.log(x);
+
